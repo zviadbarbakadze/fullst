ab83a0a2820541d774a2913d0e831c5bfc595cf5
unknown <zviadbarbakadze5@gmail.com>
2024-08-16 15:40:13 +0400

    create upvotes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 import express from "express";
 
+
 const app = express();
 app.use(express.json());
-app.post("/", (req, res) => {
-  res.send(`HELLO ${req.body.name}`);
-});
+let articlesInfo=[
+  {
+    name:"learn-react",
+    upvotes:0
+  },
+  {
+    name:"learn-node",
+    upvotes:0
+  },
+  {
+    name:"mongodb",
+    upvotes:0
+  }
+]
+app.put('/api/articles/:name/upvote',(req,res)=>{
+  const{name}=req.params
+  const article=articlesInfo.find(article=>article.name===name)
+  if(article){
+   article.upvotes=article.upvotes+1
+   res.send(` upvotes is ${article.upvotes}`)
+  }else{
+    res.send('article doesn\'t exist')
+  }
+  
+})
 
 app.listen(8000, () => {
   console.log("server is listening ");
